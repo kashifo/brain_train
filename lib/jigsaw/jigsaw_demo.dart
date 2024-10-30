2f5ff6e151b0bec2185be07b6a725f68cb25033c
kashifo <koderkashif@gmail.com>
2024-10-31 02:20:39 +0530

    Fixed issue in 'Classic Puzzle', made it compatible for Desktop and responsive.

--- lib/jigsaw/jigsaw_demo.dart
+++ lib/jigsaw/jigsaw_demo.dart
@@ -18,58 +18,102 @@ class _JigsawDemoState extends State<JigsawDemo> {
   @override
   Widget build(BuildContext context) {
     final puzzleKey = GlobalKey<JigsawWidgetState>();
+    var scrWidth = MediaQuery.of(context).size.width;
+    var scrHeight = MediaQuery.of(context).size.height;
+    print('scrWidth=$scrWidth, scrHeight=$scrHeight');
+
+    var gridWidth = scrWidth-32;
+    var gridHeight = scrHeight-150;
+
+    //w=533,h=771, w=773,h=771
+    //w=583,h=809 to w=h (809) w is 72% of height
+    if(scrHeight < scrWidth){
+      gridWidth = 0.6 * scrHeight;
+
+      /*if(scrHeight+(0.3*scrWidth) < scrWidth){
+        print('if(scrHeight+(0.3*scrWidth) < scrWidth)');
+        gridWidth = 0.4*scrHeight;
+      }*/
+      if(scrWidth-scrHeight < 0.7*scrWidth){
+        print('if(scrWidth-scrHeight < 0.7*scrWidth)');
+        gridWidth = 0.5*scrHeight;
+      }
+
+      if(scrHeight < 0.42*scrWidth){
+        print('if(scrHeight < 0.4*scrWidth)');
+        gridWidth = 0.4 * scrHeight;
+      }
+    } else if(scrWidth < scrHeight){
+      gridWidth = scrWidth;
+
+      //scrHeight doesn't have 30% more than scrWidth
+      if(scrHeight-scrWidth < 0.7*scrHeight){
+        print('if(scrHeight-scrWidth < 0.7*scrHeight){');
+        gridWidth = 0.6*scrHeight;
+      }
+    }
 
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: simpleAppBar(widget.appBarTitle, context),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(24),
+          padding: const EdgeInsets.all(20),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        puzzleImg = getRandomPic();
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          puzzleImg = getRandomPic();
+                          puzzleKey.currentState!.reset();
+                        });
+                      },
+                      child: const Text('New'),
+                    ),
+                    const SizedBox(width: 16),
+                    ElevatedButton(
+                      onPressed: () async {
+                        await puzzleKey.currentState!.generate();
+                      },
+                      child: const Text('Generate'),
+                    ),
+                    const SizedBox(width: 16),
+                    ElevatedButton(
+                      onPressed: () {
                         puzzleKey.currentState!.reset();
-                      });
-                    },
-                    child: const Text('New'),
-                  ),
-                  const SizedBox(width: 16),
-                  ElevatedButton(
-                    onPressed: () async {
-                      await puzzleKey.currentState!.generate();
-                    },
-                    child: const Text('Generate'),
-                  ),
-                  const SizedBox(width: 16),
-                  ElevatedButton(
-                    onPressed: () {
-                      puzzleKey.currentState!.reset();
-                    },
-                    child: const Text('Clear'),
-                  ),
-                ],
+                      },
+                      child: const Text('Reset'),
+                    ),
+                  ],
+                ),
               ),
-              JigsawPuzzle(
-                gridSize: 3,
-                image: AssetImage(puzzleImg),
-                onFinished: () {
-                  // ignore: avoid_print
-                  print('finished!');
-                },
-                // ignore: avoid_redundant_argument_values
-                snapSensitivity: .5,
-                // Between 0 and 1
-                puzzleKey: puzzleKey,
-                onBlockSuccess: () {
-                  // ignore: avoid_print
-                  print('block success!');
-                },
+              Center(
+                child: SizedBox(
+                  width: gridWidth,
+                  // height: gridHeight,
+                  child: JigsawPuzzle(
+                    gridSize: 3,
+                    image: AssetImage(puzzleImg),
+                    onFinished: () {
+                      // ignore: avoid_print
+                      print('finished!');
+                    },
+                    // ignore: avoid_redundant_argument_values
+                    snapSensitivity: .5,
+                    // Between 0 and 1
+                    puzzleKey: puzzleKey,
+                    onBlockSuccess: () {
+                      // ignore: avoid_print
+                      print('block success!');
+                    },
+                  ),
+                ),
               )
             ],
           ),
